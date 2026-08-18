5200a5ebe17882a72548e2e811fa4e80d7c785af
harsh24 <harsh242042004@gmail.com>
2026-08-19 00:08:21 +0530

    just commit

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from utils.logger import logger
 from wait_for_db import wait_for_db
 from workout_tracker import create_app
 
+
 logger.info("Importing app module (Flask app factory).")
 app = create_app(init_db=False)
 
